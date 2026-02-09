59a6520168099eeb9df826f51173316a4086e0a1
sh0rk1n <sh0rk1n@mail.ru>
2026-02-09 23:16:15 +0300

    chore: tuning  vite config

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,15 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
-  base: "/vue-project/",
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const base = mode === "production" ? "/vue-project/" : "/";
+  return {
+    plugins: [vue(), vueDevTools(), tailwindcss()],
+    base,
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
 });
